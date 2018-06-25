605ecc1aee889984f4283675da7b7b87cdb989cb
arpit <akparmar@isystango.com>
2018-06-25 16:57:05 +0530

    Imported UserOutput and UserInput, created state for the username and added nameChangeHandler.

--- assignment_1/src/App.js
+++ assignment_1/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserOutput from './UserOutput/UserOutput';
+import UserInput from './UserInput/UserInput';
 
 class App extends Component {
+  state = {
+    username: 'Arpit'
+  };
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <UserOutput username={this.state.username} />
+        <UserInput username={this.state.username} onNameChange={this.nameChangeHandler} />
+        <UserOutput username={'Aditya'} />
       </div>
     );
   }
